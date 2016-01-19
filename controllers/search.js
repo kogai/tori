270b65a0327ccc091169ba45876322e7df18e817
小飼慎一 <kogaishinichi@kogaishinichi-no-MacBook-Pro.local>
2016-01-20 07:55:28 +0900

    [add] 発言の掃除

--- controllers/search.js
+++ controllers/search.js
@@ -15,6 +15,47 @@ export function search(bot, msg, tori) {
       console.log(err);
       return bot.reply(msg, err.message);
     }
-    res.body.posts.forEach((p)=> bot.reply(msg, normalizePost(p)));
+
+    if (res.body.posts.length <= 0) {
+      return bot.reply(msg, `\`${msg.text}\` では記事が見つかりませんでした`);
+    }
+
+    const replies = res.body.posts.map((p)=> {
+      return new Promise((resolve, reject)=> {
+        bot.reply(msg, normalizePost(p), (replyError)=> {
+          if (replyError) {
+            return reject(replyError);
+          }
+          resolve();
+        });
+      });
+    });
+
+    Promise.all(replies).then(()=> {
+      const TIMEOUT = process.env.NODE_ENV === 'development' ? 1000 : 1000 * 60 * 3; // ms * s * m
+      setTimeout(()=> {
+        const conditinos = {
+          channel: msg.channel,
+          count: 100,
+        };
+
+        bot.api.channels.history(conditinos, (historyError, historyRes)=> {
+          if (historyError || !historyRes.ok) {
+            return bot.reply(msg, historyError.message);
+          }
+
+          const botMessages = historyRes.messages.filter((m)=> m.user === bot.identity.id);
+          botMessages.forEach((m)=> {
+            bot.api.chat.delete({
+              channel: msg.channel,
+              ts: m.ts,
+            });
+          });
+        });
+      }, TIMEOUT);
+    })
+    .catch((repliesError)=> {
+      bot.reply(msg, repliesError.message);
+    });
   });
 }
